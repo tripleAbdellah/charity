a308ac9a19c30b9d868f8db7e7ff52829fa4adcd
tripleAbdellah <abdellah.alazhar@gmail.com>
2015-04-01 17:00:07 +0100

    Added more fields
    Please find the comments in the code

--- WebApplication7/Models/QurbaniAllocation.cs
+++ WebApplication7/Models/QurbaniAllocation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +9,10 @@ namespace DonorApp.Models
     {
         public string Country { get; set; }
         public string Animal { get; set; }
+        //AAA: the user can enter (free text) notes for Qurbani donation
+        public string QurbaniNotes { get; set; }
+        //AAA: the user can enter another name (on Behalf of) for the Qurbani donations
+        public string QurbaniOnBehalf { get; set; }
 
         public QurbaniAllocation(string Country, string Animal, float UnitPrice)
             : base("Qurbani", UnitPrice)
@@ -17,10 +21,12 @@ namespace DonorApp.Models
             this.Animal = Animal;
         }
 
-        public QurbaniAllocation(string Country, string Animal, float UnitPrice, int Units) : base("Qurbani", UnitPrice, Units)
+        public QurbaniAllocation(string Country, string Animal, float UnitPrice, int Units, String QurbaniNotes, String QurbaniOnBehalf) : base("Qurbani", UnitPrice, Units)
         {
             this.Country = Country;
             this.Animal = Animal;
+            this.QurbaniNotes = QurbaniNotes;
+            this.QurbaniOnBehalf = QurbaniOnBehalf; 
         }
     }
 }
