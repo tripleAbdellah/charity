b16cf74aa45f0d603fa98881a69d4d4b6360a847
tripleAbdellah <abdellah.alazhar@gmail.com>
2015-04-01 17:10:25 +0100

    Update AllocationsController.cs

--- WebApplication7/Controllers/AllocationsController.cs
+++ WebApplication7/Controllers/AllocationsController.cs
@@ -21,5 +21,30 @@ namespace DonorApp.Controllers
             IEnumerable<Allocation> Allocations = repository.GetAllAllocations();
             return Allocations;
         }
+        
+        
+        //To retrieve orphan details donor_code is required, otherwise Orphans are not able to be retrieved 
+
+        [HttpGet]
+        [Route("{DonorCode:int}", Name = "GetAllocationsByDonorCode")]
+        public IEnumerable<Allocation> GetAllAllocations(int DonorCode)
+        {
+            IEnumerable<Allocation> Allocations = null; 
+            try
+            {
+               Allocations = repository.GetAllAllocations(DonorCode);
+            }
+            catch (NoOrphansFoundException)
+            {
+                //not sure what to do; because not all Donors have orphans
+                //perhaps return the allocations without the orphans detail? 
+                return Allocations; 
+              
+            }
+
+            return Allocations;
+        }
+        
+        
     }
 }
